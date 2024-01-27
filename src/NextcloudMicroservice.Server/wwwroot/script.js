5c3093fb3d4962c778bbf1bd1105ff7f73ba4131
splitt3r <splitt3r@users.noreply.github.com>
2024-01-27 10:46:47 +0100

    correlate traces to metrics

--- src/NextcloudMicroservice.Server/wwwroot/script.js
+++ src/NextcloudMicroservice.Server/wwwroot/script.js
@@ -3,10 +3,30 @@ response.then(response => {
     return response.json();
 }).then(weatherforecasts => {
     document.getElementById('content').innerHTML = `<div id="app-navigation">
+        <nav id="app-navigation-test">
+            <ul>
+                <li class="app-navigation-entry active">
+                    <a href="#">
+                        <span class="icon-home"></span>
+                        <span>Home</span>
+                    </a>
+                </li>
+            </ul>
+        </nav>
     </div>
     <div id="app-content" class="app-dotnet_microservice">
-        <p>
-            Number of forecasts: ${weatherforecasts.length}
-        </p>
+        <div id="app-content-wrapper">
+            <div class="app-content-list">
+                <a href="#" class="app-content-list-item">
+                    <div class="app-content-list-item-icon" style="background-color: rgb(31, 192, 216);">M</div>
+                    <div class="app-content-list-item-line-one">Important mail is very important! Don't ignore me</div>
+                    <div class="app-content-list-item-line-two">Hello there, here is an important mail from your mom</div>
+                </a>
+                </a>
+            </div>
+            <div class="app-content-detail">
+                Number of forecasts: ${weatherforecasts.length}
+            </div>
+        </div>
     </div>`;
 });
